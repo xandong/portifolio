a1ac11181cfb8a4e600eafd45c0d06ab6198550e
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-03-01 13:33:00 -0300

    navbar opacity
    navbar opacity '0' in overflow;
    overflow hidden from body;

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,5 @@
 
+const navMenu = document.querySelector('#vMenu')
 const navItems = document.querySelector("#navbar-items");
 const burguer = document.querySelector(".navbar-responsive");
 const line1 = document.querySelector("#line1");
@@ -9,15 +10,17 @@ let stts = false;
 
 click = () => {
     if(!stts) {
-        navItems.style.transform = "translateX(0)";
         stts = true;
+        navItems.style.transform = "translateX(0)";
+        navItems.style.opacity = "100%";
         line1.style.transform = "rotate(-45deg) translate(-8px, 8px)";
         line2.style.opacity = "0";
         line3.style.transform = "rotate(45deg) translate(-5px, -7px)";
     }
     else {
-        navItems.style.transform = "translateX(100%)";
         stts = false;
+        navItems.style.transform = "translateX(100%)";
+        navItems.style.opacity = "0";
         line1.style.transform = "rotate(0deg) translate(0, 0)";
         line2.style.opacity = "100%";
         line3.style.transform = "rotate(0deg) translate(0, 0)";
